0de01d0c1f3feb2cd6b4a8e30447ca8a34b4921d
SamFems <samueloluwaferanmi37@gmail.com>
2025-07-25 05:33:16 +0100

    add all

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import Comment from '../components/community/Comment'
-import CommentForm from '../components/community/CommentForm'
-import CreateThreadForm from '../components/community/CreateThreadForm'
-import ThreadCard from '../components/community/ThreadCard'
+import { Comment, CommentForm, CreateThreadForm, ThreadCard } from '../components/community'
 
 const Community = () => {
   return (
